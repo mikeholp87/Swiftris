7f20c05e07fd4d22c45b1fe133ece9ca7ef7715f
Mike Holp <mikeh@connecttel.com>
2014-09-23 20:25:20 -0500

    first commit

--- Swiftris/GameViewController.swift
+++ Swiftris/GameViewController.swift
@@ -9,61 +9,130 @@
 import UIKit
 import SpriteKit
 
-extension SKNode {
-    class func unarchiveFromFile(file : NSString) -> SKNode? {
-        if let path = NSBundle.mainBundle().pathForResource(file, ofType: "sks") {
-            var sceneData = NSData.dataWithContentsOfFile(path, options: .DataReadingMappedIfSafe, error: nil)
-            var archiver = NSKeyedUnarchiver(forReadingWithData: sceneData)
-            
-            archiver.setClass(self.classForKeyedUnarchiver(), forClassName: "SKScene")
-            let scene = archiver.decodeObjectForKey(NSKeyedArchiveRootObjectKey) as GameScene
-            archiver.finishDecoding()
-            return scene
-        } else {
-            return nil
-        }
-    }
-}
+var scene: GameScene!
+var swiftris: Swiftris!
+var panPointReference:CGPoint?
 
-class GameViewController: UIViewController {
+class GameViewController: UIViewController, SwiftrisDelegate, UIGestureRecognizerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if let scene = GameScene.unarchiveFromFile("GameScene") as? GameScene {
-            // Configure the view.
-            let skView = self.view as SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
+        
+        let skView = view as SKView
+        skView.multipleTouchEnabled = false
+        
+        scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFill
+        
+        scene.tick = didTick
+        swiftris = Swiftris()
+        swiftris.delegate = self
+        swiftris.beginGame()
+        
+        skView.presentScene(scene)
+    }
+    
+    func didTick() {
+        swiftris.letShapeFall()
+    }
+    
+    func nextShape() {
+        let newShapes = swiftris.newShape()
+        if let fallingShape = newShapes.fallingShape {
+            scene.addPreviewShapeToScene(newShapes.nextShape!) {}
+            scene.movePreviewShape(fallingShape) {
+                self.view.userInteractionEnabled = true
+                scene.startTicking()
+            }
         }
     }
-
-    override func shouldAutorotate() -> Bool {
-        return true
-    }
-
-    override func supportedInterfaceOrientations() -> Int {
-        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
-            return Int(UIInterfaceOrientationMask.AllButUpsideDown.toRaw())
+    
+    func gameDidBegin(swiftris: Swiftris) {
+        levelLabel.text = "\(swiftris.level)"
+        scoreLabel.text = "\(swiftris.score)"
+        scene.tickLengthMillis = TickLengthLevelOne
+        
+        if swiftris.nextShape != nil && swiftris.nextShape!.blocks[0].sprite == nil {
+            scene.addPreviewShapeToScene(swiftris.nextShape!) {
+                self.nextShape()
+            }
         } else {
-            return Int(UIInterfaceOrientationMask.All.toRaw())
+            nextShape()
         }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+    
+    func gameDidEnd(swiftris: Swiftris) {
+        view.userInteractionEnabled = false
+        scene.stopTicking()
+        scene.playSound("gameover.mp3")
+        scene.animateCollapsingLines(swiftris.removeAllBlocks(), fallenBlocks: Array<Array<Block>>()) {
+            swiftris.beginGame()
+        }
+    }
+    
+    func gameDidLevelUp(swiftris: Swiftris) {
+        levelLabel.text = "\(swiftris.level)"
+        if scene.tickLengthMillis >= 100 {
+            scene.tickLengthMillis -= 100
+        }else if scene.tickLengthMillis > 50 {
+            scene.tickLengthMillis -= 50
+        }
+        scene.playSound("levelup.mp3")
+    }
+    
+    func gameShapeDidDrop(swiftris: Swiftris) {
+        scene.stopTicking()
+        scene.redrawShape(swiftris.fallingShape!) {
+            swiftris.letShapeFall()
+        }
+        scene.playSound("drop.mp3")
+    }
+    
+    func gameShapeDidLand(swiftris: Swiftris) {
+        scene.stopTicking()
+        self.view.userInteractionEnabled = false
+        let removedLines = swiftris.removeCompletedLines()
+        if removedLines.linesRemoved.count > 0 {
+            self.scoreLabel.text = "\(swiftris.score)"
+            scene.animateCollapsingLines(removedLines.linesRemoved, fallenBlocks: removedLines.fallenBlocks) {
+                self.gameShapeDidLand(swiftris)
+            }
+            scene.playSound("bomb.mp3")
+        }else{
+            nextShape()
+        }
+    }
+    
+    func gameShapeDidMove(swiftris: Swiftris) {
+        scene.redrawShape(swiftris.fallingShape!) {}
     }
 
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
+    
+    @IBAction func didTap(sender: UITapGestureRecognizer) {
+        swiftris.rotateShape()
+    }
+    
+    @IBAction func didPan(sender: UIPanGestureRecognizer) {
+        let currentPoint = sender.translationInView(self.view)
+        if let originalPoint = panPointReference {
+            if abs(currentPoint.x - originalPoint.x) > (BlockSize * 0.9) {
+                if sender.velocityInView(self.view).x > CGFloat(0) {
+                    swiftris.moveShapeRight()
+                    panPointReference = currentPoint
+                }else{
+                    swiftris.moveShapeLeft()
+                    panPointReference = currentPoint
+                }
+            }
+        }else if sender.state == .Began {
+            panPointReference = currentPoint
+        }
+    }
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet weak var levelLabel: UILabel!
 }
---
+++ Swiftris/TShape.swift
@@ -0,0 +1,27 @@
+//
+//  TShape.swift
+//  Swiftris
+//
+//  Created by Mike Holp on 9/23/14.
+//  Copyright (c) 2014 Mike Holp. All rights reserved.
+//
+
+class TShape:Shape {
+    override var blockRowColumnPositions: [Orientation: Array<(columnDiff: Int, rowDiff: Int)>] {
+        return [
+            Orientation.Zero:       [(1, 0), (0, 1), (1, 1), (2, 1)],
+            Orientation.Ninety:     [(2, 1), (1, 0), (1, 1), (1, 2)],
+            Orientation.OneEighty:  [(1, 2), (0, 1), (1, 1), (2, 1)],
+            Orientation.TwoSeventy: [(0, 1), (1, 0), (1, 1), (1, 2)]
+        ]
+    }
+    
+    override var bottomBlocksForOrientations: [Orientation: Array<Block>] {
+        return [
+            Orientation.Zero:       [blocks[SecondBlockIdx], blocks[ThirdBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.Ninety:     [blocks[FirstBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.OneEighty:  [blocks[FirstBlockIdx], blocks[SecondBlockIdx], blocks[FourthBlockIdx]],
+            Orientation.TwoSeventy: [blocks[FirstBlockIdx], blocks[FourthBlockIdx]]
+        ]
+    }
+}
